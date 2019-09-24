3603055b67ad9d1cb5a37fea1ecb8929453ed069
Kade Christlieb <kchristlieb@gmail.com>
2019-09-24 13:37:42 -0600

    created two for loops to generate the buttons for the array and from the user input.

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -3,8 +3,25 @@ $(document).ready(function() {
     var sports = ["basketball", "football", "soccer", "tennis", "bowling"];
 
     // loop to create the buttons
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
+    for (let i = 0; i < sports.length; i++) {
+        var sportsBtn = $("<button>");
+        sportsBtn.addClass("sports-button");
+        sportsBtn.attr("data-name", sports[i]);
+        sportsBtn.text(sports[i]);
+        $("#buttons-view").append(sportsBtn);
+    }
+
+    // function to create new buttons when user submits the request
+    function renderButtons() {
+
+        $("#buttons-view").empty();
+        
+        for (let i = 0; i < sports.length; i++) {
+            var newBtn = $("<button>");
+            newBtn.addClass("sports-button");
+            newBtn.attr("data-name", sports[i]);
+            newBtn.text(sports[i]);
+            $("#buttons-view").append(newBtn);
         
     }
 
