855f9bba257ce4b0e4cc78b835d6b2b5cb22c802
Kade Christlieb <kchristlieb@gmail.com>
2019-09-24 13:18:05 -0600

    finished the css that ties to the direct HTML doc. Changed topic to sports and updated the heading in the jumbotron. Started the JS with global var and for loop to create the buttons.

---
+++ assests/javascript/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    
+    var sports = ["basketball", "football", "soccer", "tennis", "bowling"];
+
+    // loop to create the buttons
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
+
+})
